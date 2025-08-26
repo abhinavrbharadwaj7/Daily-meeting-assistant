28db13d32b116de750705767b5e2ce777f319469
abhinavrbharadwaj7 <navabhi51852@gmail.com>
2025-08-26 22:49:17 +0530

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ['@babel/plugin-transform-typescript', { isTSX: true }]
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  server: {
-    host: true,
-    port: 3000
-  }
 });
